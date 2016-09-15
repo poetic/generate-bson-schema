e3776a87aeab845c4e25b5865dcfc2826fdfb658
Chun-Yang <yang2007chun@gmail.com>
2016-09-15 13:48:53 -0500

    enable bson

--- src/schemas/bson.js
+++ src/schemas/bson.js
@@ -1,15 +1,123 @@
-// NOTE: json is just a subset of bson, so we can just wrap bson here
-// Modules
-var Type = require('type-of-is')
+// BSON types
+// Type                    Number       Alias
+// Double                  1            “double”
+// String                  2            “string”
+// Object                  3            “object”
+// Array                   4            “array”
+// BinaryData              5            “binData”
+// Undefined               6            “undefined”
+// ObjectId                7            “objectId”
+// Boolean                 8            “bool”
+// Date                    9            “date”
+// Null                    10           “null”
+// RegularExpression       11           “regex”
+// DBPointer               12           “dbPointer”
+// JavaScript              13           “javascript”
+// Symbol                  14           “symbol”
+// JavaScript(withScope)   15           “javascriptWithScope”
+// 32-bitInteger           16           “int”
+// Timestamp               17           “timestamp”
+// 64-bitInteger           18           “long”
+// MinKey                  -1           “minKey”
+// MaxKey                  127          “maxKey”
 
-function typeForJsonSchema (value) {
-  var type = Type.string(value).toLowerCase();
+var types = [
+  {
+    alias: 'double',
+    check: function (v, type, bsontype) { return bsontype === 'Double' }
+  },
+  {
+    alias: 'string',
+    check: function (v, type) { return type === 'string' }
+  },
+  {
+    alias: 'array',
+    check: function (v) { return Array.isArray(v) }
+  },
+  {
+    alias: 'binData',
+    check: function (v, type, bsontype) { return bsontype === 'Binary' }
+  },
+  // {
+  //   alias: 'undefined',
+  //   check: function (v, type) { return type === 'undefined' }
+  // },
+  {
+    alias: 'objectId',
+    check: function (v, type, bsontype) { return bsontype === 'ObjectID' }
+  },
+  {
+    alias: 'bool',
+    check: function (v, type) { return type === 'boolean' }
+  },
+  {
+    alias: 'date',
+    check: function (v) { return v instanceof Date }
+  },
+  // {
+  //   alias: 'null',
+  //   check: function (v) { return v === null }
+  // },
+  {
+    alias: 'regex',
+    check: function (v, type, bsontype) { return bsontype === 'BSONRegExp' }
+  },
+  {
+    alias: 'dbPointer',
+    check: function (v, type, bsontype) { return bsontype === 'DBRef' }
+  },
+  // { alias: 'javascript', check: function (v) { } },
+  {
+    alias: 'symbol',
+    check: function (v, type, bsontype) { return bsontype === 'Symbol' }
+  },
+  // { alias: 'javascriptWithScope', check: function (v) { return  } },
+  {
+    alias: 'int',
+    check: function (v, type, bsontype) { return bsontype === 'Int32' }
+  },
+  {
+    alias: 'timestamp',
+    check: function (v, type, bsontype) { return bsontype === 'Timestamp' }
+  },
+  {
+    alias: 'long',
+    check: function (v, type, bsontype) { return bsontype === 'Long' }
+  },
+  // { alias: 'minKey', check: function (v) { return  } },
+  // { alias: 'maxKey', check: function (v) { return  } },
+  // NOTE: object should be at the end
+  {
+    alias: 'object',
+    check: function (v, type) { return type === 'object' }
+  },
+]
 
-  if (type === 'date') {
-    return 'string';
+// recognize bson types
+function typeOf (value) {
+  if (value === null) {
+    return 'null'
   }
 
-  return type;
+  var type = typeof value
+
+  if (type === 'undefined') {
+    return 'undefined'
+  }
+
+  var bsontype = value._bsontype
+
+  var i
+  for (i = 0; i < types.length; i++) {
+    if (types[i].check(value, type, bsontype)) {
+      return types[i].alias
+    }
+  }
+  // Unrecognized type
+  throw {
+    message: 'This value has a type we do not recognize',
+    value: value
+  }
 }
 
 // Constants
@@ -52,13 +160,13 @@ function processArray (array, output, nested) {
     }
   } else {
     output = output || {}
-    output.type = typeForJsonSchema(array)
+    output.type = typeOf(array)
     output.items = output.items || {}
   }
 
   // Determine whether each item is different
   for (var index = 0, length = array.length; index < length; index++) {
-    var elementType = typeForJsonSchema(array[index])
+    var elementType = typeOf(array[index])
 
     if (type && elementType !== type) {
       output.items.oneOf = []
@@ -78,7 +186,7 @@ function processArray (array, output, nested) {
   if (typeof output.items.oneOf !== 'undefined' || type === 'object') {
     for (var index = 0, length = array.length; index < length; index++) {
       var value = array[index]
-      var itemType = typeForJsonSchema(value)
+      var itemType = typeOf(value)
       var required = []
       var processOutput
 
@@ -117,13 +225,13 @@ function processObject (object, output, nested) {
     }
   } else {
     output = output || {}
-    output.type = typeForJsonSchema(object)
+    output.type = typeOf(object)
     output.properties = output.properties || {}
   }
 
   for (var key in object) {
     var value = object[key]
-    var type = typeForJsonSchema(value)
+    var type = typeOf(value)
 
     if (type === 'undefined') {
       type = 'null'
@@ -163,7 +271,7 @@ module.exports = function (title, object) {
   }
 
   // Set initial object type
-  output.type = typeForJsonSchema(object)
+  output.type = typeOf(object)
 
   // Process object
   switch (output.type) {
@@ -189,3 +297,4 @@ module.exports = function (title, object) {
   // Output
   return output
 }
+
